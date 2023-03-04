9c169216dc3af1718e1abb18efd3e67622c6900f
Roman Jancik <romanj85@users.noreply.github.com>
2023-03-04 14:14:03 +0100

    MOTION_SKIP_PERIOD as environment variable

--- src/main.cpp
+++ src/main.cpp
@@ -9,19 +9,28 @@ using namespace std;
 int main(int argc, char* argv[]) {
     string webhookUrl;
     char* webhookPtr = std::getenv("MOTION_WEBHOOK");
+    char* skipPeriodPtr = std::getenv("MOTION_SKIP_PERIOD");
+    int skipPeriod = 2;
 
-    if (webhookPtr == nullptr) {
-        if (argc != 2) {
-            cout << "Usage:\n\t" << argv[0] << " <webhook url>\n\n"
-                 << "or using enviroment variable:\n\tMOTION_WEBHOOK=<webhook url> " << argv[0] << endl;
+    if (!webhookPtr) {
+        if (argc < 2) {
+            cout << "Usage:\n\t" << argv[0] << " <webhook url> [skip period seconds]\n\n"
+                 << "or using enviroment variable:\n\tMOTION_WEBHOOK=<webhook url> MOTION_SKIP_PERIOD=[seconds] " << argv[0] << endl;
             return 1;
         }
         webhookPtr = argv[1];
     }
     webhookUrl = string(webhookPtr);
+    if (!skipPeriodPtr) {
+        if (argc == 3) {
+            skipPeriod = std::atoi(argv[2]);
+        }
+    } else {
+        skipPeriod = std::atoi(skipPeriodPtr);
+    }
 
     MotionDetect md;
-    MotionRestCall rest(webhookUrl, std::chrono::seconds(2));
+    MotionRestCall rest(webhookUrl, std::chrono::seconds(skipPeriod));
     rest.call("off");
     md.run(rest);
 
